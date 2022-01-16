80c01eb8515abedb9cbaf963244f434c1633c9bc
David Granados <davidgranados73@gmail.com>
2022-01-16 17:56:23 -0500

    ch7: Advance user registration

--- accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-
-from accounts import views
-
-urlpatterns = [
-    path("", include("django.contrib.auth.urls")),
-    path("signup/", views.SignupPageView.as_view(), name="signup"),
-]
--- config/settings.py
+++ config/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
     # Third party apps
     "crispy_forms",
+    "allauth",
+    "allauth.account",
     # Local apps
     "accounts",
     "pages",
@@ -140,12 +143,28 @@ STATICFILES_FINDERS = [
 AUTH_USER_MODEL = "accounts.CustomUser"
 
 
-# Auth app config
-
-LOGIN_REDIRECT_URL = "home"
-LOGOUT_REDIRECT_URL = "home"
-
-
 # django-crispy-forms
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+
+# django-allauth
+
+LOGIN_REDIRECT_URL = "home"
+
+LOGOUT_REDIRECT_URL = "home"
+
+SITE_ID = 1
+
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
