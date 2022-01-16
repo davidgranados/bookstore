8f861d10dae36c015875efcbab7f6047648eaa33
David Granados <davidgranados73@gmail.com>
2022-01-15 22:23:39 -0500

    ch 3: Init Bookstore Project

---
+++ accounts/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+
+User = get_user_model()
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        models = User
+        fields = ("email", "username")
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ("email", "username")
