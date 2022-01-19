7d95bf86836e11a8d8e738d459493655e111e1d4
David Granados <davidgranados73@gmail.com>
2022-01-18 20:10:29 -0500

    ch12: File/Image uploads

--- config/settings.py
+++ config/settings.py
@@ -136,7 +136,8 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
-
+MEDIA_URL = "/media/"
+MEDIA_ROOT = str(BASE_DIR.joinpath("media"))
 
 # Custom user model
 
