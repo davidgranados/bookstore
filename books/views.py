eb8cfef8812cb8bb565a7c42238b6e9d0e27d71a
David Granados <davidgranados73@gmail.com>
2022-01-18 19:25:09 -0500

    ch10: Books App

---
+++ books/views.py
@@ -0,0 +1,17 @@
+from re import template
+
+from django.views.generic import DetailView, ListView
+
+from books.models import Book
+
+
+class BookListView(ListView):
+    model = Book
+    context_object_name = "book_list"
+    template_name = "books/book_list.html"
+
+
+class BookDetailView(DetailView):
+    model = Book
+    context_object_name = "book"
+    template_name = "books/book_detail.html"
